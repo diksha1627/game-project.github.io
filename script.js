0eafa56606e575657c6d28ee91bd39eab7d8ca71
Diksha <dikshabuxani84745@gmail.com>
2022-01-10 16:31:58 +0530

    added

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 score=0;
-crose= true;
+cross= true;
 
 audio=new Audio('music.mp3');
 audiogo=new Audio('gameover.mp3');
 setTimeout(()=>
 {
-   audio.play()
+   audio.play();
 },1000);
 document.onkeydown = function(e)
 {
@@ -30,7 +30,7 @@ document.onkeydown = function(e)
     {
         dino=document.querySelector('.dino');
         dinoX=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
-        dino.style.left=dinoX-112+"px";
+        dino.style.left=(dinoX-112)+"px";
         
     }
 }
@@ -43,20 +43,21 @@ setInterval(()=>
     dx=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
     dy=parseInt(window.getComputedStyle(dino,null).getPropertyValue('top'));
 
-    ox=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
-    oy=parseInt(window.getComputedStyle(dino,null).getPropertyValue('top'));
+    ox=parseInt(window.getComputedStyle(obstacle,null).getPropertyValue('left'));
+    oy=parseInt(window.getComputedStyle(obstacle,null).getPropertyValue('top'));
     
     offsetX=Math.abs(dx-ox);
     offsetY=Math.abs(dy-oy);
     
-    if(offsetX<73 &&  offsetY<52)
+    if(offsetX < 73 &&  offsetY < 52)
     {
         gameOver.innerHTML="Game Over-Reload to Play Again"
-        obstacle.classList.remove('.obstacleAni')
+        obstacle.classList.remove('obstacleAni')
         audiogo.play();
         setTimeout(()=>
         {
            audiogo.pause();
+           audio.pause();
         },1000);
     }
     else if(offsetX<145 && cross)
@@ -69,7 +70,7 @@ setInterval(()=>
         },1000);
         setTimeout(()=>
         {
-            aniDur=parseFloat(window.getComputedStyle(dino,null).getPropertyValue('animation-duration'));
+            aniDur=parseFloat(window.getComputedStyle(obstacle,null).getPropertyValue('animation-duration'));
             newDur=aniDur-0.1;
             obstacle.style.animationDuration=newDur+'s';
         },500);
@@ -78,6 +79,6 @@ setInterval(()=>
 
 function updateScore(score)
 {
-    scoreCont.innerHTML="Your Score: "+score
+    scoreCont.innerHTML="Your Score: "+score;
 }
 
